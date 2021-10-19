ed9a56e5d840b5fb5eec577ff8b1f758ddbd2284
Laura Powles <laura@laurapowles.com>
2021-10-19 18:20:46 +1100

    added a method for converting BitBoardTypes into Pieces

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -1,8 +1,15 @@
+use std::error::Error;
+
+use strum::Display;
 /// Refer to https://en.wikipedia.org/wiki/Bitboard
 use strum::EnumIter;
 use BitBoardType::*;
 
-#[derive(EnumIter, Hash, PartialEq, Eq)]
+use crate::piece::Color::*;
+use crate::piece::Piece;
+use crate::piece::PieceType::*;
+
+#[derive(EnumIter, Display, Hash, PartialEq, Eq)]
 pub enum BitBoardType
 {
     WhitePawns,
@@ -19,15 +26,39 @@ pub enum BitBoardType
     BlackKings,
 }
 
+impl BitBoardType
+{
+    pub fn get_piece(&self) -> Result<Piece, Box<dyn Error>>
+    {
+        match &self {
+            WhitePawns => Ok(Piece { color: White, piece: Pawn }),
+            WhiteKnights => Ok(Piece { color: White, piece: Knight }),
+            WhiteBishops => Ok(Piece { color: White, piece: Bishop }),
+            WhiteRooks => Ok(Piece { color: White, piece: Rook }),
+            WhiteQueens => Ok(Piece { color: White, piece: Queen }),
+            WhiteKings => Ok(Piece { color: White, piece: King }),
+            BlackPawns => Ok(Piece { color: Black, piece: Pawn }),
+            BlackKnights => Ok(Piece { color: Black, piece: Knight }),
+            BlackBishops => Ok(Piece { color: Black, piece: Bishop }),
+            BlackRooks => Ok(Piece { color: Black, piece: Rook }),
+            BlackQueens => Ok(Piece { color: Black, piece: Queen }),
+            BlackKings => Ok(Piece { color: Black, piece: King }),
+            _ =>
+                Err(format!("BitBoardType {} has no associated Piece!", &self)
+                    .into()),
+        }
+    }
+}
+
 #[derive(Debug, PartialEq)]
 pub struct BitBoard
 {
-    board: u64,
+    pub bits: u64,
 }
 
 impl BitBoard
 {
-    pub fn new(bits: u64) -> Self { BitBoard { board: bits } }
+    pub fn new(bits: u64) -> Self { BitBoard { bits } }
     pub fn empty() -> Self { Self::new(0) }
     pub fn default_from_type(bitboard_type: &BitBoardType) -> Self
     {
