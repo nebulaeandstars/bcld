62421872f46f592dc5bb39c09bbb5b8be23c65fb
Laura Powles <laura@laurapowles.com>
2021-10-19 20:20:10 +1100

    added structs to support more specific game states

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,20 @@ mod piece;
 
 use board::{BitBoardState, GameState};
 
+pub struct Square
+{
+    file: u8,
+    rank: u8,
+}
+
+pub enum CastleAvailability
+{
+    Full,
+    QueensideOnly,
+    KingsideOnly,
+    None,
+}
+
 pub fn run()
 {
     let board = BitBoardState::start_of_game();
