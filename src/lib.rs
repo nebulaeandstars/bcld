0f262b198fe104d1f8c7b19cc52c258fff7fac12
Laura Powles <laura@laurapowles.com>
2021-10-19 14:58:12 +1100

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+pub fn add(a: isize, b: isize) -> isize { a + b }
+pub fn run() { println!("Hello, world!") }
+
+#[cfg(test)]
+mod tests
+{
+    use super::add;
+
+    #[test]
+    fn test_add()
+    {
+        assert_eq!(add(2, 3), 5);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+fn main()
+{
+    // run the main function
+    bcld::run();
+}
