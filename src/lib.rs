dba8fe189bbe582ff6f51c14121ad20a160c631f
Laura Powles <laura@laurapowles.com>
2021-10-22 19:24:31 +1100

    implemented FromStr for CastleAvailability

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ mod bitboard;
 mod board;
 mod piece;
 
+use std::str::FromStr;
+
 use board::{BitBoardState, GameState};
 
 pub struct Square
@@ -14,6 +16,46 @@ pub struct CastleAvailability
 {
     white: (bool, bool),
     black: (bool, bool),
+}
+
+impl FromStr for CastleAvailability
+{
+    type Err = Box<dyn std::error::Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err>
+    {
+        if s.len() > 4 {
+            return Err("invalid castle availability length!".into());
+        }
+
+        let mut out =
+            CastleAvailability { white: (false, false), black: (false, false) };
+
+        for c in s.chars() {
+            let result: Result<(), Self::Err> = match c {
+                'K' => Ok(out.white.0 = true),
+                'Q' => Ok(out.white.1 = true),
+                'k' => Ok(out.black.0 = true),
+                'q' => Ok(out.black.1 = true),
+                _ =>
+                    Err(format!("unknown symbol {} in castle availability!", c)
+                        .into()),
+            };
+            result?;
+        }
+
+        Ok(out)
+    }
+}
+
+impl FromStr for Square
+{
+    type Err = Box<dyn std::error::Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err>
+    {
+        unimplemented!();
+    }
 }
 
 impl std::fmt::Display for CastleAvailability
@@ -66,4 +108,28 @@ pub fn run()
     let board = BitBoardState::start_of_game();
 
     println!("{}", board);
+}
+
+#[cfg(test)]
+mod tests
+{
+    use std::str::FromStr;
+
+    use super::CastleAvailability;
+
+    #[test]
+    fn test_castle_availability_from_string()
+    {
+        let tests = vec!["KQkq", "Kkq", "kq", "q"];
+
+        for test in tests {
+            assert_eq!(
+                CastleAvailability::from_str(test).unwrap().to_string(),
+                test
+            )
+        }
+
+        assert!(CastleAvailability::from_str("invalid").is_err());
+        assert!(CastleAvailability::from_str("KQKQKQ").is_err())
+    }
 }
